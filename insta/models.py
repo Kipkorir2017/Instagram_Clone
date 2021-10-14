ba050a979565515a4f837a5117a73369d379f769
Kipkorir2017 <pkobenj41@gmail.com>
2021-10-14 21:43:53 +0300

    add Image model in the models file

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image_name = models.CharField(max_length=80,blank=True)
+    image_caption = models.CharField(max_length=600)
+    profile = models.ForeignKey(on_delete = models.CASCADE)
+    likes = models.ManyToManyField(related_name='likes', blank=True)
+    comments = models.CharField(max_length=30,blank=True)
+    
+
+ 
