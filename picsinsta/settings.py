0db58995c2a2f241af9ee9e309bf3778ed953c64
Kipkorir2017 <pkobenj41@gmail.com>
2021-10-14 21:17:11 +0300

    add db and change Time_zone

--- picsinsta/settings.py
+++ picsinsta/settings.py
@@ -75,11 +75,12 @@ WSGI_APPLICATION = 'picsinsta.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'insta',
+        'USER': 'moringa',
+    'PASSWORD':'Benja312',
     }
 }
 
@@ -108,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
