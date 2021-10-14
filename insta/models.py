c81ce17afca6c79f838e3490a49597826c3d3418
Kipkorir2017 <pkobenj41@gmail.com>
2021-10-14 21:59:57 +0300

    add methods for save and delete in each model

--- insta/models.py
+++ insta/models.py
@@ -7,6 +7,18 @@ class Profile(models.Model):
     bio = models.TextField(max_length=500, default="Bio", blank=True)
     
 
+    def __str__(self):
+        return f'{self.user.user} Image'
+
+    def save_profile(self):
+        self.user
+
+    def delete_profile(self):
+        self.delete()
+
+
+
+
 class Image(models.Model):
     image_name = models.CharField(max_length=80,blank=True)
     image_caption = models.CharField(max_length=600)
@@ -14,10 +26,28 @@ class Image(models.Model):
     likes = models.ManyToManyField(related_name='likes', blank=True)
     comments = models.CharField(max_length=30,blank=True)
 
+    def __str__(self):
+        return self.image_name
+
+    def save_image(self):
+        return self.save()
+    
+    def delete_image(self):
+        return self.delete()
+
 
 class Comment(models.Model):
     comment = models.TextField()
     post= models.ForeignKey(Image, on_delete=models.CASCADE)
     user= models.ForeignKey(Profile, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True, null=True)
- 
+    
+
+    def __str__(self):
+        return f'{self.user.user} Image'
+
+    def save_comment(self):
+        self.user
+
+    def delete_comment(self):
+        self.delete()
