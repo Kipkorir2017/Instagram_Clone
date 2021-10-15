5c4f37f2783ec806df4901ee3978ddec0dd9cf3c
Kipkorir2017 <pkobenj41@gmail.com>
2021-10-15 13:59:48 +0300

    add other forms

--- insta/forms.py
+++ insta/forms.py
@@ -3,9 +3,35 @@ from django.contrib.auth.models import User
 from insta.models import Comment, Image, Profile
 from django.contrib.auth.forms import UserCreationForm
 
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+class UpdateUserForm(forms.ModelForm):
+    email = forms.EmailField(max_length=254, help_text='Required.')
+    class Meta:
+        model = User
+        fields = ('username', 'email')
 
 class  NewPostForm(forms.ModelForm):
     class Meta:
         model = Image
         exclude = ['profile', 'likes','comments']
 
+class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['bio']
+
+        
+class CommentForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['comment'].widget.attrs['placeholder'] = 'Write a comment'
+
+    class Meta:
+        model = Comment
+        fields = ('comment',)
