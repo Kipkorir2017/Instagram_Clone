c9f4dd1f8c5a1e07e2ad1bd543b23f4bcdd0958b
Kipkorir2017 <pkobenj41@gmail.com>
2021-10-15 07:39:05 +0300

    add tests for every model and add changes to the models

--- insta/models.py
+++ insta/models.py
@@ -1,20 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
+
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     profile_picture = models.ImageField(upload_to = 'images/')
     bio = models.TextField(max_length=500, default="Bio", blank=True)
     
-
     def __str__(self):
-        return f'{self.user.user} Image'
+        return self.user.username
 
     def save_profile(self):
-        self.user
+        self.save()
 
     def delete_profile(self):
-        self.delete()
+        self.delete()  
 
     def update_profile(cls, id):
         Profile.objects.get(user_id=id)
@@ -28,38 +29,38 @@ class Profile(models.Model):
 class Image(models.Model):
     image_name = models.CharField(max_length=80,blank=True)
     image_caption = models.CharField(max_length=600)
-    profile = models.ForeignKey(Profile,on_delete = models.CASCADE)
-    likes = models.ManyToManyField(User,related_name='likes', blank=True)
+    image_profile = models.ForeignKey(Profile,on_delete = models.CASCADE)
+    likes = models.ManyToManyField(User, related_name='likes', blank=True)
     comments = models.CharField(max_length=30,blank=True)
+    image=models.ImageField(upload_to='images/')
 
     def __str__(self):
-        return self.image_name
+        return self.image_name    
 
     def save_image(self):
         return self.save()
-    
+
     def delete_image(self):
-        return self.delete()
+        self.delete()
 
     @classmethod
-    def search_post(cls, name):
+    def search_image(cls, name):
         return cls.objects.filter(image_name__image__name__icontains=name)
     
     @classmethod
-    def get_picture_by_id(cls, picture_id):
-        picture = cls.objects.get(id=picture_id)
-        return picture
+    def get_image_by_id(cls, image_id):
+        image = cls.objects.get(id=image_id)
+        return image
         
     def add_likes(self):
-        return self.likes.count()
-
+        return self.likes.count()    
 
 class Comment(models.Model):
     comment = models.TextField()
     post= models.ForeignKey(Image, on_delete=models.CASCADE)
     user= models.ForeignKey(Profile, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True, null=True)
-    
+
 
     def __str__(self):
         return f'{self.user.user} Image'
--- insta/tests.py
+++ insta/tests.py
@@ -1,14 +1,16 @@
 from django.test import TestCase
+
 from .models import Comment, Profile, Image
 from django.contrib.auth.models import User
-# Create your tests here.
+
+
 class ProfileTestCase(TestCase):
     
     def setUp(self):
-        self.user = User(username='kipkorir')
+        self.user = User(username='Kipkorir')
         self.user.save()
 
-        self.profile = Profile(id=12 ,profile_picture='image.jpg', bio='This is your profile',
+        self.profile = Profile(id=12 ,profile_picture='image.jpg', bio='Test profile',
                                     user=self.user)
 
     def test_instance(self):
@@ -17,10 +19,60 @@ class ProfileTestCase(TestCase):
     def test_save_profile(self):
         self.profile.save_profile()
         profile = Profile.objects.all()
-        self.assertTrue(len(profile) > 0)
+        self.assertTrue(len(profile) >0)
 
     def test_update_profile(self):
         self.profile.save_profile()
         self.profile.update_profile(self.profile.user_id)
         self.profile.save_profile()
         self.assertTrue(Profile,self.profile.user)
+
+class ImageTestClass(TestCase):
+   
+    def setUp(self):
+        self.user = User.objects.create_user("username", "password")
+        self.new_profile = Profile(id = 12,profile_picture='image.png',bio='Test profile',user=self.user)
+        self.new_profile.save()
+        self.new_image = Image(image='image.png',image_caption="image", image_profile=self.new_profile)
+
+    def test_instance_true(self):
+        self.assertTrue(isinstance(self.new_image, Image))
+
+    def test_save_image(self):
+        self.new_image.save_image()
+        img = Image.objects.all()
+        self.assertTrue(len(img) == 1)
+
+    def test_delete_imae(self):
+        self.new_image.save_image()
+        img = Profile.objects.all()
+        self.assertTrue(len(img) <= 1)
+
+    def test_get_image_by_id(self):
+        self.new_image.save_image()
+        img = self.new_image.get_image_by_id(self.new_image.id)
+        images = Image.objects.filter(id=self.new_image.id)
+        self.assertTrue(img, images)    
+
+class CommentTestCase(TestCase):
+    def setUp(self):
+        self.user = User(username='Kipkorir')
+        self.user.save()
+
+        self.new_profile = Profile(profile_picture='image.png',bio='Test profile description',user=self.user)
+        self.new_profile.save()
+        self.new_image = Image(image='image.png',image_caption="image", image_profile=self.new_profile)
+        self.comment = Comment(comment='Your Best Image', user=self.new_profile, post = self.new_image, date="14-10-2021")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.comment, Comment))
+
+    def test_save_comment(self):
+        self.comment.save_comment()
+        comment = Comment.objects.all()
+        self.assertFalse(len(comment) > 1)
+
+    def test_delete_comment(self):
+        self.comment.save_comment()
+        comment = Comment.objects.all()
+        self.assertTrue(len(comment)  <= 1)
